60bf1f5a52138f8f61b9adc8f0b92abacf43a8b3
Vincci-Zee Ching <chingc0606@gmail.com>
2023-06-12 00:28:30 +0800

    :card_file_box: Connect database

--- server/config/Database.js
+++ server/config/Database.js
@@ -1,5 +1,8 @@
+const { Sequelize } = require('sequelize');
 const mysql = require('mysql2');
+require('dotenv').config();
 
+// Create a MySQL connection
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
@@ -7,6 +10,7 @@ const connection = mysql.createConnection({
     database: process.env.DB_NAME,
 });
 
+// Connect to the MySQL database
 connection.connect((err) => {
     if (err) {
         console.error('Error connecting to MySQL database:', err);
@@ -15,4 +19,17 @@ connection.connect((err) => {
     console.log('Connected to MySQL database!');
 });
 
-module.exports = connection;
+// Create a Sequelize instance using the MySQL connection
+const sequelize = new Sequelize({
+    dialect: 'mysql',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+
+module.exports = {
+    sequelize,
+    connection,
+};
--- server/index.js
+++ server/index.js
@@ -1,17 +1,27 @@
 require('dotenv').config();
 
-
-const db = require('./config/Database.js');
-
 const express = require('express');
+const { sequelize } = require('./config/Database.js');
+// const { UserModel } = require('./model/UserModel.js');
+// const { FarmingModel } = require('./model/FarmingModel.js');
+// const { CropModel } = require('./model/CropModel.js');
+
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Welcome to Agribrain System!');
 });
 
-const port = process.env.APP_PORT;
+// Sync the UserModel with the database
+sequelize.sync()
+    .then(() => {
+        console.log('Model synchronized with database');
+    })
+    .catch((error) => {
+        console.error('Error synchronizing UserModel:', error);
+    });
 
+const port = process.env.APP_PORT;
 app.listen(port, () => {
     console.log(`Server up and running on port ${port}...`);
 });
