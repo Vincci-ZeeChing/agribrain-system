b229d613501c9d1c04ab84a5f513a2ad6cb1e245
Vincci-Zee Ching <chingc0606@gmail.com>
2023-06-11 23:18:31 +0800

    :sparkles: Create User model and table

--- server/controller/User.js
+++ server/controller/User.js
@@ -3,16 +3,18 @@ import argon2 from "argon2";
 
 
 const getUser = async (req, res) => {}
-module.exports = getUser;
 const getUserById = async (req, res) => {}
-module.exports = getUserById;
 
 const createUser = async (req, res) => {}
-module.exports = createUser;
 
 const updateUser = async (req,res) =>{}
-module.exports = updateUser;
 
 const deleteUser =async (req,res) =>{}
-module.exports = deleteUser;
 
+module.exports = {
+    getUser,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
+};
