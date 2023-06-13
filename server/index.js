9fbc0bd196cb28c691b9dbccac116569b8d0d0bd
Vincci-Zee Ching <chingc0606@gmail.com>
2023-06-13 17:50:43 +0800

    :zap: Improve performance

--- server/index.js
+++ server/index.js
@@ -5,9 +5,9 @@ const session = require('express-session');
 const SessionStore = require('connect-session-sequelize')(session.Store);
 
 const { sequelize } = require('./config/Database.js');
-const { UserModel } = require('./model/UserModel.js');
-const { FarmingModel } = require('./model/FarmingModel.js');
-const { CropModel } = require('./model/CropModel.js');
+// const { UserModel } = require('./model/UserModel.js');
+// const { FarmingModel } = require('./model/FarmingModel.js');
+// const { CropModel } = require('./model/CropModel.js');
 const UserRoute = require("./route/UserRoute");
 const CropRoute = require("./route/CropRoute");
 const FarmingRoute = require("./route/FarmingRoute");
@@ -36,15 +36,15 @@ app.get('/', (req, res) => {
     res.send('Welcome to Agribrain System!');
 });
 
-
-// Sync the with the database
-sequelize.sync()
-    .then(() => {
-        console.log('Model synchronized with database');
-    })
-    .catch((error) => {
-        console.error('Error synchronizing UserModel:', error);
-    });
+//
+// // Sync the with the database
+// sequelize.sync()
+//     .then(() => {
+//         console.log('Model synchronized with database');
+//     })
+//     .catch((error) => {
+//         console.error('Error synchronizing UserModel:', error);
+//     });
 
 
 app.use(express.json());
