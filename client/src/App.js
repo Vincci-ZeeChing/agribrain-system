219b0c36ff0f79d805d1cb9064a3f6eb83f6b136
Vincci-Zee Ching <chingc0606@gmail.com>
2023-06-14 21:51:01 +0800

    :lipstick: Add Dashboard Page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Login from "./component/authentication/Login";
 import Agribrain from "./page/agribrain/Agribrain";
+import Dashboard from "./page/dashboard/Dashboard";
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
 
                 {/*Main Page*/}
                 <Route path="/agribrain" element={<Agribrain/>}/>
+                <Route path="/dashboard" element={<Dashboard/>}/>
+
             </Routes>
         </BrowserRouter>
     </div>
