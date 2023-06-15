b9a38126499c83c265e354b9b379384eca3ac521
Vincci-Zee Ching <chingc0606@gmail.com>
2023-06-15 22:21:32 +0800

    :lipstick: Change table display order

--- client/src/component/precision/PrecisonFarming.jsx
+++ client/src/component/precision/PrecisonFarming.jsx
@@ -76,27 +76,6 @@ const PrecisionFarming = () => {
                 </thead>
             </table>
 
-        {/*    Crop Table */}
-            <h1 className="subtitle is-underlined">Crop Listing</h1>
-            <table className="table is-bordered is-hoverable is-fullwidth mb-6">
-                <thead>
-                <tr style={{ backgroundColor: '#E1F6F0' }}>
-                    <th className="has-text-centered">No</th>
-                    <th className="has-text-centered">Crop Name</th>
-                    <th className="has-text-centered">Created By</th>
-                </tr>
-                </thead>
-                <tbody>
-                {crop.map((crop, index) => (
-                    <tr key={crop.crop_uuid}>
-                        <td className="has-text-centered">{index + 1}</td>
-                        <td>{crop.crop_name}</td>
-                        <td className="has-text-centered">{crop.USER_T.user_fullname}</td>
-                    </tr>
-                ))}
-                </tbody>
-            </table>
-
             {/* Farming Table */}
             <h1 className="subtitle is-underlined">Farming Listing</h1>
             <table className="table is-bordered is-hoverable is-fullwidth">
@@ -148,6 +127,27 @@ const PrecisionFarming = () => {
                     </div>
                 )}
             </div>
+
+            {/* Crop Table */}
+            <h1 className="subtitle is-underlined">Crop Listing</h1>
+            <table className="table is-bordered is-hoverable is-fullwidth mb-6">
+                <thead>
+                <tr style={{ backgroundColor: '#E1F6F0' }}>
+                    <th className="has-text-centered">No</th>
+                    <th className="has-text-centered">Crop Name</th>
+                    <th className="has-text-centered">Created By</th>
+                </tr>
+                </thead>
+                <tbody>
+                {crop.map((crop, index) => (
+                    <tr key={crop.crop_uuid}>
+                        <td className="has-text-centered">{index + 1}</td>
+                        <td>{crop.crop_name}</td>
+                        <td className="has-text-centered">{crop.USER_T.user_fullname}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
         </div>
     );
 };
