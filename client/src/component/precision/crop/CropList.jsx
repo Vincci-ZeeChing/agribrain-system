3cedaf4c62d017514af0b1646a33824d011aef54
Vincci-Zee Ching <chingc0606@gmail.com>
2023-06-16 11:11:06 +0800

    :lipstick: Add crop filter

--- client/src/component/precision/crop/CropList.jsx
+++ client/src/component/precision/crop/CropList.jsx
@@ -7,7 +7,8 @@ import Switch from "react-switch";
 
 const CropList = () => {
     const [crop, setCrop] = useState([]);
-    const [filterText, setFilterText] = useState("");
+    const [userFilterText, setUserFilterText] = useState("");
+    const [cropFilterText, setCropFilterText] = useState("");
     const [deleteConfirmation, setDeleteConfirmation] = useState(null);
     const { user } = useSelector((state) => state.auth);
 
@@ -26,12 +27,24 @@ const CropList = () => {
         getCrops();
     };
 
-    const handleFilterChange = (e) => {
-        setFilterText(e.target.value);
+    const handleUserFilterChange = (e) => {
+        setUserFilterText(e.target.value);
     };
 
-    const filteredName = crop.filter((crop) => {
-        return crop.USER_T.user_fullname.toLowerCase().includes(filterText.toLowerCase());
+    const handleCropFilterChange = (e) => {
+        setCropFilterText(e.target.value);
+    };
+
+    const filteredCrops = crop.filter((crop) => {
+        return (
+            crop.USER_T.user_fullname.toLowerCase().includes(userFilterText.toLowerCase())
+        );
+    });
+
+    const filteredCropsByCrop = filteredCrops.filter((crop) => {
+        return (
+            crop.crop_name.toLowerCase().includes(cropFilterText.toLowerCase())
+        );
     });
 
     const showDeleteConfirmation = (cropId) => {
@@ -63,22 +76,39 @@ const CropList = () => {
                 </Link>
             </div>
 
-            <div className="field mt-5">
-                <label className="label">Filter by user</label>
+            <div className="field is-grouped mt-5">
                 <div className="control">
-                    <p className="control has-icons-left">
+                    <label className="label">Filter by User</label>
+                    <div className="control has-icons-left">
                         <input
                             type="text"
                             className="input is-rounded"
-                            value={filterText}
-                            onChange={handleFilterChange}
+                            value={userFilterText}
+                            onChange={handleUserFilterChange}
                             placeholder="Enter Name"
                             style={{ minWidth: "20vw", maxWidth: "25vw" }}
                         />
                         <span className="icon is-small is-left">
                             <FaFilter className="filter-icon" />
                         </span>
-                    </p>
+                    </div>
+                </div>
+
+                <div className="control">
+                    <label className="label">Filter by Crop</label>
+                    <div className="control has-icons-left">
+                        <input
+                            type="text"
+                            className="input is-rounded"
+                            value={cropFilterText}
+                            onChange={handleCropFilterChange}
+                            placeholder="Enter Crop Name"
+                            style={{ minWidth: "20vw", maxWidth: "25vw" }}
+                        />
+                        <span className="icon is-small is-left">
+                            <FaFilter className="filter-icon" />
+                        </span>
+                    </div>
                 </div>
             </div>
 
@@ -89,24 +119,24 @@ const CropList = () => {
                         <th className="has-text-centered">No</th>
                         <th className="has-text-centered">Crop Name</th>
                         <th className="has-text-centered">Created By</th>
-                        <th className="has-text-centered">Active</th>
-                        {user && (user.user.user_role === 'Farmer' || user.user.user_role === 'Admin') && (
-                            <th className="has-text-centered">Actions</th>
+                        <th className="has-text-centered">Action</th>
+                        {user && (user.user.user_role === "Farmer" || user.user.user_role === "Admin") && (
+                            <th className="has-text-centered">Active</th>
                         )}
                     </tr>
                     </thead>
                     <tbody>
-                    {filteredName.length === 0 ? (
+                    {filteredCropsByCrop.length === 0 ? (
                         <tr>
                             <td colSpan="4">No data found.</td>
                         </tr>
                     ) : (
-                        filteredName.map((crop, index) => (
+                        filteredCropsByCrop.map((crop, index) => (
                             <tr key={crop.crop_uuid}>
                                 <td className="has-text-centered">{index + 1}</td>
                                 <td>{crop.crop_name}</td>
                                 <td className="has-text-centered">{crop.USER_T.user_fullname}</td>
-                                {user && (user.user.user_role === 'Farmer' || user.user.user_role === 'Admin') && (
+                                {user && (user.user.user_role === "Farmer" || user.user.user_role === "Admin") && (
                                     <td>
                                         <div className="has-text-centered">
                                             <Link
@@ -119,8 +149,10 @@ const CropList = () => {
                                                 onClick={() => showDeleteConfirmation(crop.crop_uuid)}
                                                 className="is-hoverable is-small  delete-text"
                                             >
-                                            <span className="is-underlined" style={{color:"red"}}>Delete</span>
-                                        </span>
+                                              <span className="is-underlined" style={{ color: "red" }}>
+                                                Delete
+                                              </span>
+                                            </span>
                                         </div>
                                     </td>
                                 )}
@@ -148,22 +180,17 @@ const CropList = () => {
                 <div className="modal is-active">
                     <div className="modal-background"></div>
                     <div className="modal-card">
-                        <header className="modal-card-head" style={{backgroundColor:"#71AF9D"}}>
-                            <p className="modal-card-title" style={{color:"white", textAlign:"center",fontWeight:"bold"}}>Confirm Deletion</p>
-                            <button
-                                className="delete"
-                                aria-label="close"
-                                onClick={closeDeleteConfirmation}
-                            ></button>
+                        <header className="modal-card-head" style={{ backgroundColor: "#71AF9D" }}>
+                            <p className="modal-card-title" style={{ color: "white", textAlign: "center", fontWeight: "bold" }}>
+                                Confirm Deletion
+                            </p>
+                            <button className="delete" aria-label="close" onClick={closeDeleteConfirmation}></button>
                         </header>
                         <section className="modal-card-body is-flex is-justify-content-center">
-                            <p style={{margin:"2vh"}}>Are you sure you want to delete this crop?</p>
+                            <p style={{ margin: "2vh" }}>Are you sure you want to delete this crop?</p>
                         </section>
                         <footer className="modal-card-foot  is-justify-content-end">
-                            <button
-                                className="button is-danger is-small"
-                                onClick={() => deleteCrop(deleteConfirmation)}
-                            >
+                            <button className="button is-danger is-small" onClick={() => deleteCrop(deleteConfirmation)}>
                                 Delete
                             </button>
                             <button className="button is-small" onClick={closeDeleteConfirmation}>
