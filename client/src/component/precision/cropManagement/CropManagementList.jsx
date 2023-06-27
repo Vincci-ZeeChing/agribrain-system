ab2da7a428aab4423e80dfb6189b67a5d3ff4efb
Vincci-Zee Ching <chingc0606@gmail.com>
2023-06-27 20:23:32 +0800

    :lipstick: Crop Management List

--- client/src/component/precision/cropManagement/CropManagementList.jsx
+++ client/src/component/precision/cropManagement/CropManagementList.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link} from "react-router-dom";
+import axios from "axios";
 
 const CropManagementList = () => {
+
+    const [cropManagement, setCropManagement] = useState([]);
+    const getCropsManagement = async () => {
+        const response = await axios.get("http://localhost:5000/api/v1/cropManagement");
+        setCropManagement(response.data);
+        console.log(response.data);
+    };
+
+    useEffect(() => {
+        getCropsManagement();
+    }, []);
+
+
     return (
         <div>
             <h1 className="title">Crop Management</h1>
@@ -9,7 +23,7 @@ const CropManagementList = () => {
             <Link
                 to="/precision-farming/crop-management"
                 className="button mb-2"
-                style={{ backgroundColor: '#71AF9D', color: 'white' }}
+                style={{backgroundColor: '#71AF9D', color: 'white'}}
             >
                 Add New
             </Link>
@@ -25,20 +39,37 @@ const CropManagementList = () => {
                         <th className="has-text-centered">Sold (kg)</th>
                         <th className="has-text-centered">Price (RM)</th>
                         <th className="has-text-centered">Created By</th>
+                        <th className="has-text-centered">Actions</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td className="has-text-centered">1</td>
-                        <td className="has-text-centered">2021-08-01</td>
-                        <td className="has-text-centered">Paddy</td>
-                        <td className="has-text-centered">100</td>
-                        <td className="has-text-centered">-</td>
-                        <td className="has-text-centered">100</td>
-                        <td className="has-text-centered">100</td>
-                        <td className="has-text-centered">Farmer 1</td>
-
+                {cropManagement.map((crop, index) => (
+                    <tr key={crop.c_management_uuid}>
+                        <td className="has-text-centered">{index + 1}</td>
+                        <td className="has-text-centered">{crop.c_management_date}</td>
+                        <td className="has-text-centered">{crop.CROP_T.crop_name}</td>
+                        <td className="has-text-centered">{crop.c_management_harvest}</td>
+                        <td className="has-text-centered">{crop.c_management_stored}</td>
+                        <td className="has-text-centered">{crop.c_management_sold}</td>
+                        <td className="has-text-centered">{crop.c_management_price}</td>
+                        <td className="has-text-centered">{crop.USER_T.user_fullname}</td>
+                        <td>
+                            <div className="has-text-centered">
+                                <Link
+                                    to={`/precision-farming/crop-management${crop.crop_uuid}`}
+                                    className="is-small is-info mr-3"
+                                >
+                                    <span className="is-underlined">Edit</span>
+                                </Link>
+                                <span className="is-hoverable is-small  delete-text">
+                                    <span className="is-underlined" style={{color: "red"}}>
+                                        Delete
+                                    </span>
+                                </span>
+                            </div>
+                        </td>
                     </tr>
+                ))}
                 </tbody>
 
             </table>
