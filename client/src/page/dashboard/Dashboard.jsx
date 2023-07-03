450ff81f609b170995ab4d3b0c034e5e56c13c6a
Vincci-Zee Ching <chingc0606@gmail.com>
2023-07-03 23:32:31 +0800

    :tada: Dashboard Design

--- client/src/page/dashboard/Dashboard.jsx
+++ client/src/page/dashboard/Dashboard.jsx
@@ -5,6 +5,7 @@ import {useDispatch, useSelector} from "react-redux";
 import {useNavigate} from "react-router-dom";
 import {getMe} from "../../features/authSlice"
 import {Helmet} from "react-helmet";
+import DashboardComponent from "../../component/dashboard/Dashboard";
 
 const Dashboard = () => {
     const dispatch = useDispatch();
@@ -27,6 +28,8 @@ const Dashboard = () => {
                 <title>AgriBrain | Dashboard</title>
             </Helmet>
             <Welcome/>
+
+            <DashboardComponent/>
         </Layout>
     );
 };
