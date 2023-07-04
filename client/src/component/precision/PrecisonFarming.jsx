6637c06976f4a1c7ea2665fbd4e46d16b8f74cf4
Vincci-Zee Ching <chingc0606@gmail.com>
2023-07-04 22:52:27 +0800

    :lipstick: Add three more cards

--- client/src/component/precision/PrecisonFarming.jsx
+++ client/src/component/precision/PrecisonFarming.jsx
@@ -4,6 +4,10 @@ import {ShakeLittle} from "reshake";
 import allVege from "../../image/crop/allVege2.jpg";
 import cropManagement from "../../image/crop/cropManagement.jpeg";
 import farmingRecord from "../../image/crop/farmingRecord.jpg";
+import cropList from "../../image/crop/cropList.jpg";
+import cropManagementList from "../../image/crop/cropManagementList.jpg";
+import farmingList from "../../image/crop/farmingList.jpg";
+
 import {useSelector} from "react-redux";
 
 const PrecisionFarming = () => {
@@ -86,8 +90,6 @@ const PrecisionFarming = () => {
                 )}
 
 
-
-
                 <div className="column is-4">
                     <ShakeLittle>
                         <div className="card mt-5 mb-4" onClick={() => handleCardClick('/precision-farming/farming-record/add')}>
@@ -119,84 +121,103 @@ const PrecisionFarming = () => {
                     </ShakeLittle>
                 </div>
 
+                <div className="column is-4">
+                    <ShakeLittle>
+                        <div className="card mt-5 mb-6" onClick={() => handleCardClick('/precision-farming/crop/add')}>
+                            <div style={{ position: 'relative', height:"40vh"}}>
+                                <img
+                                    src={cropList}
+                                    alt="cropList"
+                                    style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', objectFit: 'cover', zIndex: 1 }}
+                                />
+                                <div className="card-content"
+                                     style={{
+                                         position: 'relative',
+                                         zIndex: 2,
+                                         backgroundColor:"white",
+                                         width:"95%",
+                                         left:"2%",
+                                         top:"50%",
+                                         boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
+                                         opacity: 0.9,
+                                         minHeight:"24vh"
+                                     }}>
+                                    <div className="content has-text-centered">
+                                        <div className="subtitle" style={{fontWeight:"bold"}}>Crops List</div>
+                                        <div>hahahaha</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </ShakeLittle>
+                </div>
+
+                {user && (user.user.user_role === 'Farmer' || user.user.user_role === 'Admin') && (
+                    <div className="column is-4">
+                        <ShakeLittle>
+                            <div className="card mt-5 mb-4" onClick={() => handleCardClick('/precision-farming/crop-management/add')}>
+                                <div style={{ position: 'relative', height:"40vh"}}>
+                                    <img
+                                        src={cropManagementList}
+                                        alt="cropManagementList"
+                                        style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', objectFit: 'cover', zIndex: 1 }}
+                                    />
+                                    <div className="card-content"
+                                         style={{
+                                             position: 'relative',
+                                             zIndex: 2,
+                                             backgroundColor:"white",
+                                             width:"95%",
+                                             left:"2%",
+                                             top:"50%",
+                                             boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
+                                             opacity: 0.9,
+                                             minHeight:"24vh"
+                                         }}>
+                                        <div className="content has-text-centered">
+                                            <div className="subtitle" style={{fontWeight:"bold"}}>Crop Management List</div>
+                                            <div>hahahaha</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </ShakeLittle>
+                    </div>
+                )}
+
+                <div className="column is-4">
+                    <ShakeLittle>
+                        <div className="card mt-5 mb-4" onClick={() => handleCardClick('/precision-farming/farming-record/add')}>
+                            <div style={{ position: 'relative', height:"40vh"}}>
+                                <img
+                                    src={farmingList}
+                                    alt="farmingList"
+                                    style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', objectFit: 'cover', zIndex: 1 }}
+                                />
+                                <div className="card-content"
+                                     style={{
+                                         position: 'relative',
+                                         zIndex: 2,
+                                         backgroundColor:"white",
+                                         width:"95%",
+                                         left:"2%",
+                                         top:"50%",
+                                         boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
+                                         opacity: 0.9,
+                                         minHeight:"24vh"
+                                     }}>
+                                    <div className="content has-text-centered">
+                                        <div className="subtitle" style={{fontWeight:"bold"}}>Farming List</div>
+                                        <div>hahahaha</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </ShakeLittle>
+                </div>
+
+
             </div>
-
-
-
-
-
-            {/*/!* Farming Table *!/*/}
-            {/*<Link to="/precision-farming/farming-record" className="subtitle is-underlined">Farming Listing</Link>*/}
-            {/*<table className="table is-bordered is-hoverable is-fullwidth mt-3">*/}
-            {/*    <thead>*/}
-            {/*    <tr  style={{ backgroundColor: '#E1F6F0' }}>*/}
-            {/*        <th className="has-text-centered">No</th>*/}
-            {/*        <th className="has-text-centered">Farming Name</th>*/}
-            {/*        <th className="has-text-centered">Date</th>*/}
-            {/*        <th className="has-text-centered">Crop Name</th>*/}
-            {/*        <th className="has-text-centered">Created By</th>*/}
-            {/*    </tr>*/}
-            {/*    </thead>*/}
-            {/*    <tbody>*/}
-            {/*    {currentRecords.map((farming, index) =>*/}
-            {/*        renderTableRow(farming, index)*/}
-            {/*    )}*/}
-            {/*    {emptyRows > 0 && (*/}
-            {/*        Array.from({ length: emptyRows }, (_, index) => (*/}
-            {/*            <tr key={`empty-${index}`} className="empty-row">*/}
-            {/*                <td>&nbsp;</td>*/}
-            {/*                <td>&nbsp;</td>*/}
-            {/*                <td>&nbsp;</td>*/}
-            {/*                <td>&nbsp;</td>*/}
-            {/*                <td>&nbsp;</td>*/}
-            {/*            </tr>*/}
-            {/*        ))*/}
-            {/*    )}*/}
-            {/*    </tbody>*/}
-            {/*</table>*/}
-
-            {/*/!* Pagination *!/*/}
-            {/*<div className="pagination" style={{ marginTop: '1rem', justifyContent: 'flex-end' }}>*/}
-            {/*    {farming.length > recordsPerPage && (*/}
-            {/*        <div className="buttons">*/}
-            {/*            {Array.from(*/}
-            {/*                { length: Math.ceil(farming.length / recordsPerPage) },*/}
-            {/*                (_, index) => (*/}
-            {/*                    <button*/}
-            {/*                        key={index}*/}
-            {/*                        onClick={() => paginate(index + 1)}*/}
-            {/*                        className={`button ${*/}
-            {/*                            currentPage === index + 1 ? 'is-primary' : ''*/}
-            {/*                        }`}*/}
-            {/*                    >*/}
-            {/*                        {index + 1}*/}
-            {/*                    </button>*/}
-            {/*                )*/}
-            {/*            )}*/}
-            {/*        </div>*/}
-            {/*    )}*/}
-            {/*</div>*/}
-
-            {/*/!* Crop Table *!/*/}
-            {/*<Link to="/precision-farming/crop" className="subtitle is-underlined">Crop Listing</Link>*/}
-            {/*<table className="table is-bordered is-hoverable is-fullwidth mb-6 mt-3">*/}
-            {/*    <thead>*/}
-            {/*    <tr style={{ backgroundColor: '#E1F6F0' }}>*/}
-            {/*        <th className="has-text-centered">No</th>*/}
-            {/*        <th className="has-text-centered">Crop Name</th>*/}
-            {/*        <th className="has-text-centered">Created By</th>*/}
-            {/*    </tr>*/}
-            {/*    </thead>*/}
-            {/*    <tbody>*/}
-            {/*    {crop.map((crop, index) => (*/}
-            {/*        <tr key={crop.crop_uuid}>*/}
-            {/*            <td className="has-text-centered">{index + 1}</td>*/}
-            {/*            <td>{crop.crop_name}</td>*/}
-            {/*            <td className="has-text-centered">{crop.USER_T.user_fullname}</td>*/}
-            {/*        </tr>*/}
-            {/*    ))}*/}
-            {/*    </tbody>*/}
-            {/*</table>*/}
         </div>
     );
 };
