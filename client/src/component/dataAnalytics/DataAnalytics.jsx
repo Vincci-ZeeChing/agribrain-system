8c15978c53506449cc6687767c162cb170883a86
Vincci-Zee Ching <chingc0606@gmail.com>
2023-07-04 18:39:40 +0800

    :lipstick: Data Analytics Dashboard Design

--- client/src/component/dataAnalytics/DataAnalytics.jsx
+++ client/src/component/dataAnalytics/DataAnalytics.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
-import {ShakeLittle } from "reshake";
+import {ShakeLittle} from "reshake";
+
+import allVege from '../../image/crop/allVege.jpg';
+import fertiliser from '../../image/crop/fertiliser.jpg';
+import report from '../../image/crop/report.jpg';
+import visualization from "../../image/crop/visualization.jpg";
+
 
 const DataAnalytics = () => {
     const [isHovered, setIsHovered] = useState(false);
-    const [isClicked, setIsClicked] = useState(false);
 
     const handleCardHover = () => {
         setIsHovered(!isHovered);
@@ -20,18 +25,31 @@ const DataAnalytics = () => {
             <div className="columns is-multiline">
                 <div className="column is-4">
                     <ShakeLittle>
-                        <div className="card"
-                             onClick={() => handleCardClick('/data-analytics/crop-recommendation')}
-                             style={{ margin: '2vw' }}
-                        >
-                            <div className="card-content">
-                                <div className="content has-text-centered">
-                                    Soil is in good condition
+                        <div className="card mt-5 mb-4" onClick={() => handleCardClick('/data-analytics/crop-recommendation')}>
+                            <div style={{ position: 'relative', height:"40vh"}}>
+                                <img
+                                    src={allVege}
+                                    alt="allVege"
+                                    style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', objectFit: 'cover', zIndex: 1 }}
+                                />
+                                <div className="card-content"
+                                     style={{
+                                         position: 'relative',
+                                         zIndex: 2,
+                                         backgroundColor:"white",
+                                         width:"95%",
+                                         left:"2%",
+                                         top:"50%",
+                                         boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
+                                         opacity: 0.9,
+                                         minHeight:"24vh"
+                                     }}>
+                                    <div className="content has-text-centered">
+                                        <div className="subtitle" style={{fontWeight:"bold"}}>Crop Recommendation</div>
+                                        <div>Recommendation about the type of crops to be cultivated which is best suited for the respective conditions</div>
+                                    </div>
                                 </div>
                             </div>
-                            <header className="card-header" style={{ boxShadow: 'none' , backgroundColor:"#E1F6F0"}}>
-                                <p className="card-header-title is-centered">Crop Recommendation</p>
-                            </header>
                         </div>
                     </ShakeLittle>
                 </div>
@@ -39,57 +57,97 @@ const DataAnalytics = () => {
 
                 <div className="column is-4">
                     <ShakeLittle>
-                        <div className="card"
-                             onClick={() => handleCardClick('/data-analytics/fertiliser-recommendation')}
-                             style={{ margin: '2vw' }}
-                        >
-                            <div className="card-content">
-                                <div className="content has-text-centered">
-                                    Soil is in good condition
+                        <div className="card mt-5 mb-4" onClick={() => handleCardClick('/data-analytics/fertiliser-recommendation')}>
+                            <div style={{ position: 'relative', height:"40vh"}}>
+                                <img
+                                    src={fertiliser}
+                                    alt="fertiliser"
+                                    style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', objectFit: 'cover', zIndex: 1 }}
+                                />
+                                <div className="card-content"
+                                     style={{
+                                         position: 'relative',
+                                         zIndex: 2,
+                                         backgroundColor:"white",
+                                         width:"95%",
+                                         left:"2%",
+                                         top:"50%",
+                                         boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
+                                         opacity: 0.9,
+                                         minHeight:"24vh"
+                                     }}>
+                                    <div className="content has-text-centered">
+                                        <div className="subtitle"  style={{fontWeight:"bold"}}>Fertiliser Recommendation</div>
+                                        <div>Recommendation about the type of fertilizer best suited for the particular soil and the recommended crop</div>
+                                    </div>
                                 </div>
                             </div>
-                            <header className="card-header" style={{ boxShadow: 'none' , backgroundColor:"#E1F6F0"}}>
-                                <p className="card-header-title is-centered" >Fertiliser Recommendation</p>
-                            </header>
                         </div>
                     </ShakeLittle>
                 </div>
 
                 <div className="column is-4">
                     <ShakeLittle>
-                        <div className="card"
-                             onClick={() => handleCardClick('/data-analytics/report')}
-                             style={{ margin: '2vw' }}>
-                            <div className="card-content">
-                                <div className="content has-text-centered">
-                                    Soil is in good condition
+                        <div className="card mt-5 mb-4" onClick={() => handleCardClick('/data-analytics/report')}>
+                            <div style={{ position: 'relative', height:"40vh"}}>
+                                <img
+                                    src={report}
+                                    alt="report"
+                                    style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', objectFit: 'cover', zIndex: 1 }}
+                                />
+                                <div className="card-content"
+                                     style={{
+                                         position: 'relative',
+                                         zIndex: 2,
+                                         backgroundColor:"white",
+                                         width:"95%",
+                                         left:"2%",
+                                         top:"50%",
+                                         boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
+                                         opacity: 0.9,
+                                         minHeight:"24vh"
+                                     }}>
+                                    <div className="content has-text-centered">
+                                        <div className="subtitle"  style={{fontWeight:"bold"}}>Report</div>
+                                        <div>Summary of the user-inserted crop data, crop management and farming records</div>
+                                    </div>
                                 </div>
                             </div>
-                            <header className="card-header" style={{ boxShadow: 'none' , backgroundColor:"#E1F6F0"}}>
-                                <p className="card-header-title is-centered" >Report</p>
-                            </header>
                         </div>
                     </ShakeLittle>
                 </div>
 
                 <div className="column is-4">
                     <ShakeLittle>
-                        <div className="card"
-                             onClick={() => handleCardClick('/data-analytics/visualization')}
-                             style={{ margin: '2vw' }}>
-                            <div className="card-content">
-                                <div className="content has-text-centered">
-                                    Soil is in good condition
+                        <div className="card mt-5 mb-6" onClick={() => handleCardClick('/data-analytics/visualization')}>
+                            <div style={{ position: 'relative', height:"40vh"}}>
+                                <img
+                                    src={visualization}
+                                    alt="visualization"
+                                    style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', objectFit: 'cover', zIndex: 1 }}
+                                />
+                                <div className="card-content"
+                                     style={{
+                                         position: 'relative',
+                                         zIndex: 2,
+                                         backgroundColor:"white",
+                                         width:"95%",
+                                         left:"2%",
+                                         top:"50%",
+                                         boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
+                                         opacity: 0.9,
+                                         minHeight:"24vh"
+                                     }}>
+                                    <div className="content has-text-centered">
+                                        <div className="subtitle"  style={{fontWeight:"bold"}}>Visualization</div>
+                                        <div>Interactive charts and graphs displaying real-time and historical data of soil and surrounding parameters</div>
+                                    </div>
                                 </div>
                             </div>
-                            <header className="card-header" style={{ boxShadow: 'none' , backgroundColor:"#E1F6F0"}}>
-                                <p className="card-header-title is-centered" >Visualization</p>
-                            </header>
                         </div>
                     </ShakeLittle>
                 </div>
             </div>
-
         </div>
     );
 };
