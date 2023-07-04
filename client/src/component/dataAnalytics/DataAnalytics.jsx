6d3cb9a8f73f7ff39cf971f608c63dd3d9fb9c68
Vincci-Zee Ching <chingc0606@gmail.com>
2023-07-04 16:59:29 +0800

    :lipstick: Data Analytics Dashboard

--- client/src/component/dataAnalytics/DataAnalytics.jsx
+++ client/src/component/dataAnalytics/DataAnalytics.jsx
@@ -1,9 +1,95 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {ShakeLittle } from "reshake";
 
 const DataAnalytics = () => {
+    const [isHovered, setIsHovered] = useState(false);
+    const [isClicked, setIsClicked] = useState(false);
+
+    const handleCardHover = () => {
+        setIsHovered(!isHovered);
+    };
+
+    const handleCardClick = (endpoint) => {
+        window.location.href = endpoint;
+    };
+
     return (
         <div>
-            <h1 className="title">DataAnalytics</h1>
+            <h1 className="title">Data Analytics</h1>
+
+            <div className="columns is-multiline">
+                <div className="column is-4">
+                    <ShakeLittle>
+                        <div className="card"
+                             onClick={() => handleCardClick('/data-analytics/crop-recommendation')}
+                             style={{ margin: '2vw' }}
+                        >
+                            <div className="card-content">
+                                <div className="content has-text-centered">
+                                    Soil is in good condition
+                                </div>
+                            </div>
+                            <header className="card-header" style={{ boxShadow: 'none' , backgroundColor:"#E1F6F0"}}>
+                                <p className="card-header-title is-centered">Crop Recommendation</p>
+                            </header>
+                        </div>
+                    </ShakeLittle>
+                </div>
+
+
+                <div className="column is-4">
+                    <ShakeLittle>
+                        <div className="card"
+                             onClick={() => handleCardClick('/data-analytics/fertiliser-recommendation')}
+                             style={{ margin: '2vw' }}
+                        >
+                            <div className="card-content">
+                                <div className="content has-text-centered">
+                                    Soil is in good condition
+                                </div>
+                            </div>
+                            <header className="card-header" style={{ boxShadow: 'none' , backgroundColor:"#E1F6F0"}}>
+                                <p className="card-header-title is-centered" >Fertiliser Recommendation</p>
+                            </header>
+                        </div>
+                    </ShakeLittle>
+                </div>
+
+                <div className="column is-4">
+                    <ShakeLittle>
+                        <div className="card"
+                             onClick={() => handleCardClick('/data-analytics/report')}
+                             style={{ margin: '2vw' }}>
+                            <div className="card-content">
+                                <div className="content has-text-centered">
+                                    Soil is in good condition
+                                </div>
+                            </div>
+                            <header className="card-header" style={{ boxShadow: 'none' , backgroundColor:"#E1F6F0"}}>
+                                <p className="card-header-title is-centered" >Report</p>
+                            </header>
+                        </div>
+                    </ShakeLittle>
+                </div>
+
+                <div className="column is-4">
+                    <ShakeLittle>
+                        <div className="card"
+                             onClick={() => handleCardClick('/data-analytics/visualization')}
+                             style={{ margin: '2vw' }}>
+                            <div className="card-content">
+                                <div className="content has-text-centered">
+                                    Soil is in good condition
+                                </div>
+                            </div>
+                            <header className="card-header" style={{ boxShadow: 'none' , backgroundColor:"#E1F6F0"}}>
+                                <p className="card-header-title is-centered" >Visualization</p>
+                            </header>
+                        </div>
+                    </ShakeLittle>
+                </div>
+            </div>
+
         </div>
     );
 };
