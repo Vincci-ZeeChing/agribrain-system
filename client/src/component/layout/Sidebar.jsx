8fdab33ea40006512f50ed676a326f2b3862ac1b
Vincci-Zee Ching <chingc0606@gmail.com>
2023-07-04 21:49:35 +0800

    :fire: Remove Historical Interface

--- client/src/component/layout/Sidebar.jsx
+++ client/src/component/layout/Sidebar.jsx
@@ -65,7 +65,7 @@ const Sidebar = () => {
                         <a onClick={toggleClimateSubMenu}><TiWeatherPartlySunny/> Climate Condition</a>
                         {isClimateSubMenuOpen &&
                             <ul>
-                                <li className="ml-1"><NavLink to="/climate-condition/historical">Historical</NavLink></li>
+                                <li className="ml-1"><NavLink to="/climate-condition/forecast">Forecast</NavLink></li>
                             </ul>
                         }
                     </li>
