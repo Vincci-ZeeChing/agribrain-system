f3303b8f02afd80e72b09732b0ca8208b903029d
Vincci-Zee Ching <chingc0606@gmail.com>
2023-07-22 18:51:06 +0800

    :zap: Rename

--- client/src/page/agribrain/Agribrain.jsx
+++ client/src/page/agribrain/Agribrain.jsx
@@ -4,7 +4,7 @@ import {useDispatch, useSelector} from "react-redux";
 import {useNavigate} from "react-router-dom";
 import {getMe} from "../../features/authSlice"
 import {Helmet} from "react-helmet";
-import Homepage from "../../component/homepage/homepage";
+import Homepage from "../../component/homepage/Homepage";
 
 const Agribrain = () => {
     const dispatch = useDispatch();
